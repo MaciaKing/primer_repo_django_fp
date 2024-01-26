68ee58543443d0a7ac765a306f73c9ce88233d67
MaciaKing <maciasalvasalva@gmail.com>
2024-01-26 16:42:16 +0100

    [feat]: first app

---
+++ personal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+# Validate that the api is working
+def index(request):
+    response={
+        "response": "working"
+    }
+    return JsonResponse(response)
